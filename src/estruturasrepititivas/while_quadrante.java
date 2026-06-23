a1f9512e1a87a659c4e51475310682b48bd4c22d
LeonidasMario <leonidasmario57@gmail.com>
2026-06-23 16:23:49 -0300

    atualização

--- src/estruturasrepititivas/while_quadrante.java
+++ src/estruturasrepititivas/while_quadrante.java
@@ -19,10 +19,10 @@ public class while_quadrante {
 			
 			if (x > 0 && y > 0) {
 				System.out.println("primeiro");
-			}else if(x < 0 && y>0 ) {
-				System.out.println("segundo");
 			}else if (x < 0 && y<0) {
 				System.out.println("terceiro");
+			}else if(x < 0 && y>0 ) {
+				System.out.println("segundo");
 			}else {
 				System.out.println("quarto");
 			}
